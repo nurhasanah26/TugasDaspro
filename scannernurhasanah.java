80adc703a649c04fd8bcd2628be46a5e2bfd6771
nurhasanah26 <56464065+nurhasanah26@users.noreply.github.com>
2019-10-12 11:16:11 +0700

    Tugas Daspro Scanner
    Nama : Nur Hasanah
    Tif 1 G

---
+++ scannernurhasanah.java
@@ -0,0 +1,48 @@
+  
+package input.scanner;
+
+import java.util.Scanner;
+
+public class InputScanner {
+    private static Object BIODATA3;
+    public static void main(String[] args) {
+        System.out.println("ASSALAMUALAIKUM WR.WB");
+    
+    //Guarku;                                                       
+    Scanner BIODATAKU = new Scanner (System.in);    System.out.println("Guarku Si = "); 
+    String Guarku = BIODATAKU.nextLine();           System.out.println("Guarku = " + Guarku );
+    
+    
+    //Lahirku;
+    Scanner BIODATAKU1 = new Scanner (System.in);   System.out.println("Lahir Ku di =  ");
+    String Lahirku = BIODATAKU1.nextLine();         System.out.println("Lahirku = " + Lahirku);
+    
+    
+    //Tanggal Lahirku;
+    Scanner BIODATAKU2 = new Scanner ( System.in);  System.out.println("Tanggal Lahirku = ");
+    String Tanggal = BIODATAKU2.nextLine();         System.out.println("Tanggal = " + Tanggal);
+    
+    //Alamatku;
+    Scanner BIODATAKU3 = new Scanner (System.in);   System.out.println("Alamatku di = ");
+    String Alamatku = BIODATAKU3.nextLine ();       System.out.println("Alamat Ku di = " + Alamatku);
+    
+    //Hobbyku;
+    Scanner BIODATAKU4 = new Scanner (System.in);   System.out.println("Hobbyku = ");
+    String Hobbyku = BIODATAKU4.nextLine();         System.out.println("Hobbyku adalah = " + Hobbyku);
+       
+    System.out.println("WASSALAMUALAIKUM WR.WB");
+    
+       
+             
+        
+    
+    
+        
+    
+    
+    
+           
+           
+    }
+    
+}
